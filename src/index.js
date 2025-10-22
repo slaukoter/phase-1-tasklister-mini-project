2ea261fb05d098c70663d69e28e40c7aaeb762b5
Steven <slaukoter@gmail.com>
2025-10-22 13:30:13 -0500

    passes test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form')
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const taskInput = event.target.elements['new-task-description'].value
+
+    buildToDo(taskInput)
+
+    form.reset()
+  })
+
 });
+
+  function buildToDo(task){
+    const taskList = document.getElementById('tasks')
+    const li = document.createElement('li')
+    li.textContent = task
+    taskList.append(li)
+  }
